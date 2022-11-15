5eb91555f18283cc303dc615018e3d375f362cb3
EC2 Default User <ec2-user@ip-172-31-4-95.ap-northeast-1.compute.internal>
2022-11-15 10:03:29 +0000

    [finish]10章

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+before_action :configure_permitted_parameters, if: :devise_controller?
+#↑ユーザー登録・ログイン認証が行われる前にconfigure~メソッドが実行される。
+
+#サインイン・アウト後の遷移先はルートパスがデフォルト。それを変更するのが↓
+ def after_sign_in_path_for(resource) #サインイン後にどこに遷移するか設定してるメソッド
+   about_path
+ end
+
+def after_sign_out_path_for(resource)
+  about_path
 end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+end
+
+#befor_action :configure_permitted_parameters, if: :devise_controller?
